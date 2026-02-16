7a562aa108d423ef0d293820b3a845e1ad65778c
TataHani <kontaktmarcinjanczyk@gmail.com>
2026-02-16 13:08:26 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+}
+
+module.exports = nextConfig
+```
+
+4. **Kliknij "Commit changes..." → "Commit changes"**
+
+---
+
+Teraz Tailwind. **Dodaj kolejny plik:**
+
+1. **Nazwa pliku:**
+```
+   tailwind.config.js
