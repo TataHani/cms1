ec73c3ef5b4f863ba97ab6cf4786ec1e095ebf1d
TataHani <kontaktmarcinjanczyk@gmail.com>
2026-02-16 13:28:25 +0100

    Update route.js

--- src/app/api/auth/logout/route.js
+++ src/app/api/auth/logout/route.js
@@ -5,16 +5,3 @@ export async function GET() {
   cookies().delete('user_id')
   redirect('/')
 }
-```
-
-**Commit**
-
----
-
-Teraz łączymy z Vercel! 
-
-**Zrób tak:**
-
-1. **Otwórz w nowej karcie:**
-```
-   https://vercel.com/
