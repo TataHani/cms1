53cb9018177339f4663b03b36e7441a307f6786b
TataHani <kontaktmarcinjanczyk@gmail.com>
2026-02-16 13:25:51 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,21 +4,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-```
-
-5. **Commit changes**
-
----
-
-Teraz jeszcze zaktualizujmy Next.js do nowszej wersji. 
-
-**Znajdź plik `package.json`**, edytuj go i **zmień linię z next:**
-
-Zmień:
-```
-"next": "14.1.0",
-```
-
-Na:
-```
-"next": "14.2.21",
