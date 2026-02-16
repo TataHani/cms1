f410a6f5dfb71b0c95b1f976944b8751ed07be3f
TataHani <kontaktmarcinjanczyk@gmail.com>
2026-02-16 13:08:40 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+```
+
+3. **Commit**
+
+---
+
+I jeszcze jeden. **Dodaj plik:**
+
+1. **Nazwa pliku:**
+```
+   postcss.config.js
